e9fe4b259e813840f8f4c74c1dc05e805d5dcc2a
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 02:43:55 +0700

    create middlewares

---
+++ app/middlewares.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// Middlewares is
+func Middlewares(e *echo.Echo) {
+	// Remove Trailing Slash
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	// CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	// body limit
+	e.Use(middleware.BodyLimit("8M"))
+
+	// Gzip compress
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{Level: 5}))
+
+	// secure
+	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{}))
+}
