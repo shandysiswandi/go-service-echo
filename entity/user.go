6cda5364c0c31ed6fa6ae2a555fee7778ae7f267
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 21:04:37 +0700

    add validation tag

--- entity/user.go
+++ entity/user.go
@@ -15,9 +15,9 @@ const UserTable string = "users"
 // User is
 type User struct {
 	base.UUID
-	Name     string `gorm:"type:varchar(100)" json:"name" validate:"required"`
-	Email    string `gorm:"type:varchar(100)" json:"email" validate:"required,email"`
-	Password string `gorm:"type:varchar(100)" json:"password" validate:"required"`
+	Name     string `gorm:"type:varchar(100)" json:"name" validate:"required,min=5"`
+	Email    string `gorm:"type:varchar(100)" json:"email" validate:"required,email,min=5"`
+	Password string `gorm:"type:varchar(100)" json:"password" validate:"required,min=6"`
 	Task     []Task `gorm:"foreignKey:UserID" json:"tasks"`
 	base.Timestamp
 }
