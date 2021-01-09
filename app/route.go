922e2c9081079fb70d7c419613a73d2b843675af
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 18:21:17 +0700

    update routes

--- app/route.go
+++ app/route.go
@@ -3,8 +3,19 @@ package app
 import (
 	"net/http"
 
-	taskHandler "go-rest-echo/domain/task/delivery"
-	userHandler "go-rest-echo/domain/user/delivery"
+	tac "go-rest-echo/domain/task/create"
+	tad "go-rest-echo/domain/task/delete"
+	taf "go-rest-echo/domain/task/fetch"
+	tag "go-rest-echo/domain/task/get"
+	tau "go-rest-echo/domain/task/update"
+
+	usc "go-rest-echo/domain/user/create"
+	usd "go-rest-echo/domain/user/delete"
+	usf "go-rest-echo/domain/user/fetch"
+	usg "go-rest-echo/domain/user/get"
+	usu "go-rest-echo/domain/user/update"
+
+	"go-rest-echo/helper"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,20 +23,20 @@ import (
 // Route is
 func Route(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Auto Migren Pala Gue")
+		return c.String(http.StatusOK, "ENV "+helper.Env("ENV"))
 	})
 
 	task := e.Group("/tasks")
-	task.GET("", taskHandler.FetchTask)
-	task.GET("/:id", taskHandler.GetTask)
-	task.POST("", taskHandler.CreateTask)
-	task.PUT("/:id", taskHandler.UpdateTask)
-	task.DELETE("/:id", taskHandler.DeleteTask)
+	task.GET("", taf.Delivery)
+	task.GET("/:id", tag.Delivery)
+	task.POST("", tac.Delivery)
+	task.PUT("/:id", tau.Delivery)
+	task.DELETE("/:id", tad.Delivery)
 
 	user := e.Group("/users")
-	user.GET("", userHandler.FetchUser)
-	user.GET("/:id", userHandler.GetUser)
-	user.POST("", userHandler.CreateUser)
-	user.PUT("/:id", userHandler.UpdateUser)
-	user.DELETE("/:id", userHandler.DeleteUser)
+	user.GET("", usf.Delivery)
+	user.GET("/:id", usg.Delivery)
+	user.POST("", usc.Delivery)
+	user.PUT("/:id", usu.Delivery)
+	user.DELETE("/:id", usd.Delivery)
 }
