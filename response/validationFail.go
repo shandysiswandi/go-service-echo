183fe91a084b9fd77ffbdc622b7ea9e4f24bc2e3
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 21:04:58 +0700

    add dynamic message

--- response/validationFail.go
+++ response/validationFail.go
@@ -1,6 +1,7 @@
 package response
 
 import (
+	"go-rest-echo/helper"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
@@ -13,15 +14,32 @@ func ValidationFail(c echo.Context, err interface{}) error {
 
 	for _, err := range err.(validator.ValidationErrors) {
 		e = append(e, map[string]interface{}{
-			"key":     err.StructField(),
-			"message": "must be " + err.Tag(),
+			"key":     helper.SnakeCase(err.StructField()),
+			"message": getMessage(err),
 			"value":   err.Value(),
 		})
 	}
 
-	return c.JSON(http.StatusBadRequest, Error{
+	return c.JSON(http.StatusUnprocessableEntity, Error{
 		Status:  false,
 		Message: "Validation Failed",
 		Error:   e,
 	})
+}
+
+func getMessage(e validator.FieldError) (msg string) {
+	switch e.Tag() {
+	case "min":
+		msg = "value must be greater than"
+	case "required":
+		msg = "value must be required"
+	default:
+		msg = "value must be validate"
+	}
+
+	if e.Param() != "" {
+		msg = msg + " " + e.Param()
+	}
+
+	return msg
 }
