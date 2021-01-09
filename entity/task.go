8a50e7475b81fb4b72c28e7d7361b2820c969efd
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 17:51:29 +0700

    create entity task

---
+++ entity/task.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"go-rest-echo/entity/base"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// TaskTable is
+const TaskTable string = "tasks"
+
+// Task is
+type Task struct {
+	base.UUID
+	UserID      string `gorm:"type:char(36)" json:"user_id" validate:"required"`
+	Title       string `gorm:"type:varchar(100)" json:"title" validate:"required"`
+	Description string `gorm:"type:varchar(100)" json:"description" validate:"required"`
+	Completed   bool   `json:"completed"`
+	base.Timestamp
+}
+
+// BeforeCreate is
+func (u *Task) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New().String()
+	return
+}
