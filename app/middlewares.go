711aa841f14d8a638689b885870a9dbc683a60f2
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 11:18:13 +0700

    update format logger

--- app/middlewares.go
+++ app/middlewares.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,7 +14,7 @@ func Middlewares(e *echo.Echo) {
 	e.Pre(middleware.RemoveTrailingSlash())
 
 	// logger
-	e.Use(middleware.Logger())
+	logger(e)
 
 	// recover panic
 	e.Use(middleware.Recover())
@@ -33,4 +34,17 @@ func Middlewares(e *echo.Echo) {
 
 	// secure
 	e.Use(middleware.Secure())
+}
+
+func logger(e *echo.Echo) {
+	time := "\033[33mTIME\033[0m: ${time_rfc3339}"
+	method := "\033[33mMETHOD\033[0m: ${method}"
+	uri := "\033[33mURI\033[0m: ${uri}"
+	ip := "\033[33mIP\033[0m: ${remote_ip}"
+	status := "\033[33mSTATUS\033[0m: ${status}"
+	latency := "\033[33mLATENCY\033[0m: ${latency_human}"
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: fmt.Sprintf("%s | %s | %s | %s | %s | %s\n", time, method, uri, ip, status, latency),
+	}))
 }
