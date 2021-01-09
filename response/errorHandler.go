12ba565cc9c65a1be716b4843dadaafc075cb18d
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 19:24:53 +0700

    move to pacakege response

--- response/errorHandler.go
+++ response/errorHandler.go
@@ -1,8 +1,7 @@
-package helper
+package response
 
 import (
 	"errors"
-	"go-rest-echo/response"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -11,16 +10,16 @@ import (
 // HandleErrors is
 func HandleErrors(c echo.Context, e error) error {
 	if errors.Is(e, gorm.ErrRecordNotFound) {
-		return response.NotFound(c, gorm.ErrRecordNotFound)
+		return NotFound(c, gorm.ErrRecordNotFound)
 	}
 
 	if errors.Is(e, gorm.ErrInvalidTransaction) {
-		return response.NotFound(c, gorm.ErrInvalidTransaction)
+		return NotFound(c, gorm.ErrInvalidTransaction)
 	}
 
 	if errors.Is(e, gorm.ErrNotImplemented) {
-		return response.NotFound(c, gorm.ErrNotImplemented)
+		return NotFound(c, gorm.ErrNotImplemented)
 	}
 
-	return response.InternalServerError(c, e)
+	return InternalServerError(c, e)
 }
