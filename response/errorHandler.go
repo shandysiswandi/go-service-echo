ffff2c59b786989fb61b3547f46abe797d953efd
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-09 21:05:17 +0700

    catch another error

--- response/errorHandler.go
+++ response/errorHandler.go
@@ -21,5 +21,45 @@ func HandleErrors(c echo.Context, e error) error {
 		return NotFound(c, gorm.ErrNotImplemented)
 	}
 
+	if errors.Is(e, gorm.ErrMissingWhereClause) {
+		return NotFound(c, gorm.ErrMissingWhereClause)
+	}
+
+	if errors.Is(e, gorm.ErrUnsupportedRelation) {
+		return NotFound(c, gorm.ErrUnsupportedRelation)
+	}
+
+	if errors.Is(e, gorm.ErrPrimaryKeyRequired) {
+		return NotFound(c, gorm.ErrPrimaryKeyRequired)
+	}
+
+	if errors.Is(e, gorm.ErrModelValueRequired) {
+		return NotFound(c, gorm.ErrModelValueRequired)
+	}
+
+	if errors.Is(e, gorm.ErrInvalidData) {
+		return NotFound(c, gorm.ErrInvalidData)
+	}
+
+	if errors.Is(e, gorm.ErrUnsupportedDriver) {
+		return NotFound(c, gorm.ErrUnsupportedDriver)
+	}
+
+	if errors.Is(e, gorm.ErrRegistered) {
+		return NotFound(c, gorm.ErrRegistered)
+	}
+
+	if errors.Is(e, gorm.ErrInvalidField) {
+		return NotFound(c, gorm.ErrInvalidField)
+	}
+
+	if errors.Is(e, gorm.ErrEmptySlice) {
+		return NotFound(c, gorm.ErrEmptySlice)
+	}
+
+	if errors.Is(e, gorm.ErrDryRunModeUnsupported) {
+		return NotFound(c, gorm.ErrDryRunModeUnsupported)
+	}
+
 	return InternalServerError(c, e)
 }
