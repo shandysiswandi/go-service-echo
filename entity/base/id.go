32fa15039db5bab2390d4d0b17f4c535d7ecc2d4
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-10 12:39:20 +0700

    rename to id

--- entity/base/id.go
+++ entity/base/id.go
@@ -4,3 +4,8 @@ package base
 type Increment struct {
 	ID uint `gorm:"primaryKey" json:"id"`
 }
+
+// UUID is
+type UUID struct {
+	ID string `gorm:"primaryKey; type:varchar(36)" json:"id"`
+}
--- entity/base/uuid.go
+++
@@ -1,6 +0,0 @@
-package base
-
-// UUID is
-type UUID struct {
-	ID string `gorm:"primaryKey; type:varchar(36)" json:"id"`
-}
