59322ced30b225915a8f050060f395902d290b58
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-10 12:15:15 +0700

    fix tag reflec

--- entity/task.go
+++ entity/task.go
@@ -14,8 +14,8 @@ const TaskTable string = "tasks"
 type Task struct {
 	base.UUID
 	UserID      string `gorm:"type:varchar(36)" json:"user_id" validate:"required"`
-	Title       string `gorm:"type:varchar(100)" json:"title" validate:"required, min=5"`
-	Description string `gorm:"type:varchar(100)" json:"description" validate:"required, min=15"`
+	Title       string `gorm:"type:varchar(100)" json:"title" validate:"required,min=5"`
+	Description string `gorm:"type:varchar(100)" json:"description" validate:"required,min=15"`
 	Completed   bool   `json:"completed"`
 	base.Timestamp
 }
