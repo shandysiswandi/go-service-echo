69f1c12cae9d75a3359493cc36e995b3263395c8
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-10 12:13:59 +0700

    update to simple

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 
-	"go-rest-echo/app"
+	"go-rest-echo/app/context"
+	"go-rest-echo/app/middleware"
+	"go-rest-echo/app/route"
+	"go-rest-echo/app/validation"
 	"go-rest-echo/database/mysql"
-	"go-rest-echo/entity"
 	"go-rest-echo/helper"
 
 	"github.com/joho/godotenv"
@@ -13,31 +15,25 @@ import (
 )
 
 func main() {
-	config()
-	db()
-	start()
-}
-
-func config() {
+	/* --------------- Start Configuration --------------- */
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
-}
 
-func db() {
-	db, err := mysql.GetDB()
+	/* --------------- Start Database --------------- */
+	_, err = mysql.GetDB()
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
 
-	db.AutoMigrate(&entity.User{}, &entity.Task{})
-}
+	// db.AutoMigrate(&entity.User{}, &entity.Task{})
 
-func start() {
+	/* --------------- Start Aplication --------------- */
 	e := echo.New()
-	app.Validation(e)
-	app.Route(e)
-	app.Middleware(e)
+	context.Initialize(e)
+	validation.Initialize(e)
+	middleware.Initialize(e)
+	route.Initialize(e)
 	e.Logger.Fatal(e.Start(":" + helper.Env("PORT")))
 }
