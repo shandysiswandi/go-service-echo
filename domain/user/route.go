3a39c0ae31cde3cc04f391549ca58379068d1b7f
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-11 00:39:27 +0700

    update route user

--- domain/user/route.go
+++ domain/user/route.go
@@ -1,21 +1,25 @@
 package user
 
-import (
-	"go-rest-echo/domain/user/create"
-	"go-rest-echo/domain/user/delete"
-	"go-rest-echo/domain/user/fetch"
-	"go-rest-echo/domain/user/get"
-	"go-rest-echo/domain/user/update"
+import "github.com/labstack/echo/v4"
 
-	"github.com/labstack/echo/v4"
-)
+type router struct{}
 
-// Route is
-func Route(e *echo.Echo) {
-	r := e.Group("/users")
-	r.GET("", fetch.Delivery)
-	r.GET("/:id", get.Delivery)
-	r.POST("", create.Delivery)
-	r.PUT("/:id", update.Delivery)
-	r.DELETE("/:id", delete.Delivery)
+// Router is
+type Router interface {
+	Initialize(*echo.Echo, string)
+}
+
+// NewRouter is
+func NewRouter() Router {
+	return &router{}
+}
+
+func (ro *router) Initialize(e *echo.Echo, p string) {
+	r := e.Group(p)
+	d := NewDelivery()
+	r.GET("", d.Fetch)
+	r.GET("/:id", d.Get)
+	r.POST("", d.Create)
+	r.PUT("/:id", d.Update)
+	r.DELETE("/:id", d.Delete)
 }
