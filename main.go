519ef3709fec3b608b651fa138906486ecdcb677
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-12 20:42:38 +0700

    simple main

--- main.go
+++ main.go
@@ -1,39 +1,15 @@
 package main
 
 import (
-	"log"
-
-	"go-rest-echo/app/context"
-	"go-rest-echo/app/middleware"
-	"go-rest-echo/app/route"
-	"go-rest-echo/app/validation"
-	"go-rest-echo/database/mysql"
-	"go-rest-echo/helper"
-
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
+	"go-rest-echo/app"
+	"go-rest-echo/config"
+	"go-rest-echo/db"
 )
 
 func main() {
-	/* --------------- Start Configuration --------------- */
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error %v", err)
-	}
+	config.NewConfiguration().Start()
 
-	/* --------------- Start Database --------------- */
-	_, err = mysql.GetDB()
-	if err != nil {
-		log.Fatalf("Error %v", err)
-	}
+	db.NewDatabase().Start()
 
-	// db.AutoMigrate(&entity.User{}, &entity.Task{})
-
-	/* --------------- Start Aplication --------------- */
-	e := echo.New()
-	context.Initialize(e)
-	validation.Initialize(e)
-	middleware.Initialize(e)
-	route.Initialize(e)
-	e.Logger.Fatal(e.Start(":" + helper.Env("PORT")))
+	app.NewApplication().Start()
 }
