c5463d730312770e6b3678b290724f588cfac87b
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-13 19:48:07 +0700

    add test bcrypt

--- helper/bcrypt_test.go
+++ helper/bcrypt_test.go
@@ -1,27 +1,38 @@
 package helper_test
 
-import "testing"
+import (
+	"go-rest-echo/helper"
+	"testing"
+)
 
 const MESSAGE string = "Expected `%s`, but got %s"
 
 func TestHashPassword(t *testing.T) {
-	t.Run("1", func(t *testing.T) {
-		got := ""
-		want := ""
+	t.Run("1 Check five character from front", func(t *testing.T) {
+		want := "$2a$10"
+		got, err := helper.HashPassword("password")
+		if err != nil {
+			t.Errorf(MESSAGE, want, err.Error())
+		}
 
-		if got != want {
+		if got[0:6] != want {
 			t.Errorf(MESSAGE, want, got)
 		}
 	})
 }
 
 func TestCheckPasswordHash(t *testing.T) {
-	t.Run("1", func(t *testing.T) {
-		got := ""
-		want := ""
+	t.Run("1 Check password is valid", func(t *testing.T) {
+		val, err := helper.HashPassword("password")
+		if err != nil {
+			t.Errorf(MESSAGE, "", err.Error())
+
+		}
+		want := true
+		got := helper.CheckPasswordHash("password", val)
 
 		if got != want {
-			t.Errorf(MESSAGE, want, got)
+			t.Errorf("Expected `%v`, but got %v", want, got)
 		}
 	})
 }
