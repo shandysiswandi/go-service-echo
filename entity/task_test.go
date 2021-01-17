a0696dc91f29c2249e542235ca104d31a57ece11
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-17 13:51:41 +0700

    remove hook test

--- entity/task_test.go
+++ entity/task_test.go
@@ -3,8 +3,6 @@ package entity_test
 import (
 	"go-rest-echo/entity"
 	"testing"
-
-	"gorm.io/gorm"
 )
 
 func TestTask(t *testing.T) {
@@ -15,16 +13,6 @@ func TestTask(t *testing.T) {
 
 		if got != want {
 			t.Errorf("Expected `%v`, but got %v", want, got)
-		}
-	})
-
-	t.Run("2 Before Create Hook", func(t *testing.T) {
-		task := new(entity.Task)
-		db := new(gorm.DB)
-		got := task.BeforeCreate(db)
-
-		if got != nil {
-			t.Errorf("Expected `%v`, but got %v", nil, got)
 		}
 	})
 }
