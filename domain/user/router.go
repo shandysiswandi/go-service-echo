26e31537783b1dc94e478166ce9f6452f1617b9e
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-17 18:47:24 +0700

    rename to router

---
+++ domain/user/router.go
@@ -0,0 +1,15 @@
+package user
+
+import "github.com/labstack/echo/v4"
+
+// NewRouter is
+func NewRouter(e *echo.Echo) {
+	d := NewDelivery()
+	r := e.Group("/users")
+
+	r.GET("", d.Fetch)
+	r.GET("/:id", d.Get)
+	r.POST("", d.Create)
+	r.PUT("/:id", d.Update)
+	r.DELETE("/:id", d.Delete)
+}
