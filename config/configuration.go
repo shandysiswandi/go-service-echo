0e633cfd5aeca8d4e45bb06ee914028e3c078426
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-17 17:30:06 +0700

    simple configuration

--- config/configuration.go
+++ config/configuration.go
@@ -2,19 +2,8 @@ package config
 
 import "github.com/joho/godotenv"
 
-type config struct{}
-
-// Interface is
-type Interface interface {
-	Start()
-}
-
 // NewConfiguration is
-func NewConfiguration() Interface {
-	return &config{}
-}
-
-func (config) Start() {
+func NewConfiguration() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		panic(err)
