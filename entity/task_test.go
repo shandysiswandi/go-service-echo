0672d3af146f212e1ce4c3bc66b494bf98a0813f
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-17 13:43:53 +0700

    deleted test no 3

--- entity/task_test.go
+++ entity/task_test.go
@@ -27,28 +27,4 @@ func TestTask(t *testing.T) {
 			t.Errorf("Expected `%v`, but got %v", nil, got)
 		}
 	})
-
-	t.Run("3 Task Field", func(t *testing.T) {
-		task := new(entity.Task)
-
-		if task.ID != "" {
-			t.Errorf("Expected `%v`, but got %v", "", task.ID)
-		}
-
-		if task.UserID != "" {
-			t.Errorf("Expected `%v`, but got %v", "", task.UserID)
-		}
-
-		if task.Title != "" {
-			t.Errorf("Expected `%v`, but got %v", "", task.Title)
-		}
-
-		if task.Description != "" {
-			t.Errorf("Expected `%v`, but got %v", "", task.Description)
-		}
-
-		if task.Completed != false {
-			t.Errorf("Expected `%v`, but got %v", "", task.Completed)
-		}
-	})
 }
