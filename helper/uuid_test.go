8cae387de3a2b28f79d75c1f9d16a14a6b56a0d8
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-22 15:39:16 +0700

    refactor test uuid

--- helper/uuid_test.go
+++ helper/uuid_test.go
@@ -11,12 +11,7 @@ func TestGenerateUUID(t *testing.T) {
 	// instance assert
 	assert := assert.New(t)
 
-	// call actual function
 	actual := helper.GenerateUUID()
 
-	// define variable here
-
-	// assertion execution
-	assert.NotEqualf("", actual, "Expected `uuid string`, but got `%v`", actual)
 	assert.Equalf(36, len(actual), "Expected length = `%v`, but got `%v`", 36, len(actual))
 }
