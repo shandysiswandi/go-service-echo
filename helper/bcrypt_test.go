bc1141459afaaa002daffdcd8bb742bef4afaf1c
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-22 15:14:49 +0700

    refactor bcrypt

--- helper/bcrypt_test.go
+++ helper/bcrypt_test.go
@@ -11,14 +11,11 @@ func TestHashPassword(t *testing.T) {
 	// instance assert
 	assert := assert.New(t)
 
-	t.Run("1 Check five character from front", func(t *testing.T) {
+	t.Run("1 Check five character from front no error", func(t *testing.T) {
 		actual, err := helper.HashPassword("password")
 		expected := "$2a$10"
 
-		if err != nil {
-			t.Errorf("Expected `%s`, but got %s", expected, err.Error())
-		}
-
+		assert.Nilf(err, "Expected `%s`, but got error", expected)
 		assert.Equalf(actual[0:6], expected, "Expected `%s`, but actual %s", expected, actual)
 	})
 }
@@ -28,9 +25,7 @@ func TestCheckPasswordHash(t *testing.T) {
 	assert := assert.New(t)
 
 	t.Run("1 Check password is valid", func(t *testing.T) {
-		hash := "$2a$10$mjqqoczR7odoHg/npdnwcuJCk4GHUDYTrkX48vuy/tNq7P/V/wAGi" // password
-
-		actual := helper.CheckPasswordHash("password", hash)
+		actual := helper.CheckPasswordHash("password", "$2a$10$mjqqoczR7odoHg/npdnwcuJCk4GHUDYTrkX48vuy/tNq7P/V/wAGi")
 		expected := true
 
 		assert.Equalf(expected, actual, "Expected `%v`, but actual %v", expected, actual)
