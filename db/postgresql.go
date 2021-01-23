eb9b6b9053c1a6bfe9f32184e49099a7e6cb1a46
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-23 10:33:28 +0700

    update postgresql connection

--- db/postgresql.go
+++ db/postgresql.go
@@ -2,32 +2,24 @@ package db
 
 import (
 	"errors"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var pgCon *gorm.DB
-
-func postgresqlConnection() (err error) {
-	pgCon, err = gorm.Open(postgres.Open(os.Getenv("GORM_POSTGRESQL_DSN")), &gorm.Config{})
+func postgresqlConnection(dsn string) (db *gorm.DB, err error) {
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return errors.New("Can't connect database postgres with gorm library")
+		return nil, errors.New("Can't connect database postgres with gorm library")
 	}
 
-	sqlCon, err := pgCon.DB()
+	sqlCon, err := db.DB()
 	if err != nil {
-		return errors.New("Can't instance postgres pool database connection")
+		return nil, errors.New("Can't instance postgres pool database connection")
 	}
 
 	sqlCon.SetMaxIdleConns(5)
 	sqlCon.SetMaxOpenConns(50)
 
-	return nil
-}
-
-// GetPostgresqlDB is
-func GetPostgresqlDB() *gorm.DB {
-	return pgCon
+	return db, nil
 }
