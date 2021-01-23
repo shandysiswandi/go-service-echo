1c931f5666ca2b10a7a1bea1e459efc1f82084cd
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-23 10:33:18 +0700

    update mysql connection

--- db/mysql.go
+++ db/mysql.go
@@ -2,33 +2,25 @@ package db
 
 import (
 	"errors"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var mysqlCon *gorm.DB
-
-func mysqlConnection() (err error) {
-	mysqlCon, err = gorm.Open(mysql.Open(os.Getenv("GORM_MYSQL_DSN")), &gorm.Config{PrepareStmt: true})
+func mysqlConnection(dsn string) (db *gorm.DB, err error) {
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{PrepareStmt: true})
 	if err != nil {
-		return errors.New("Can't connect database mysql with gorm library")
+		return nil, errors.New("Can't connect database mysql with gorm library")
 	}
 
 	// pooling connection
-	sqlCon, err := mysqlCon.DB()
+	sqlCon, err := db.DB()
 	if err != nil {
-		return errors.New("Can't instance mysql pool database connection")
+		return nil, errors.New("Can't instance mysql pool database connection")
 	}
 
 	sqlCon.SetMaxIdleConns(5)
 	sqlCon.SetMaxOpenConns(50)
 
-	return nil
-}
-
-// GetMysqlDB is
-func GetMysqlDB() *gorm.DB {
-	return mysqlCon
+	return db, nil
 }
