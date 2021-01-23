5635c8b8ca145825ee4cf12b5620707d20eca9fe
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-23 10:33:09 +0700

    update mongo connection

--- db/mongo.go
+++ db/mongo.go
@@ -9,26 +9,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mongoCon *mongo.Database
-
-func mongoConnection() error {
+func mongoConnection() (*mongo.Database, error) {
 	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
 	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		return nil, err
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	err = client.Connect(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	mongoCon = client.Database(os.Getenv("MONGO_DATABASE"))
-
-	return nil
-}
-
-// GetMongoDB is
-func GetMongoDB() *mongo.Database {
-	return mongoCon
+	return client.Database(os.Getenv("MONGO_DATABASE")), nil
 }
