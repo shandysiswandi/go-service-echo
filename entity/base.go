2e954422b5a8fff0dd25456dcb88f7cd511fdfe6
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-24 12:22:06 +0700

    remove gorm tag

--- entity/base.go
+++ entity/base.go
@@ -8,17 +8,17 @@ import (
 
 // Increment is
 type Increment struct {
-	ID uint `gorm:"primaryKey" json:"id"`
+	ID uint `json:"id"`
 }
 
 // UUID is
 type UUID struct {
-	ID string `gorm:"primaryKey; type:varchar(36)" json:"id"`
+	ID string `json:"id"`
 }
 
 // Timestamps is
 type Timestamps struct {
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
