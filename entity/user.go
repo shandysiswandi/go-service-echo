fddbf7c4cef7dbbe8fd4bc1bcebec208226a641d
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-24 12:25:49 +0700

    add payload create and method setID

--- entity/user.go
+++ entity/user.go
@@ -1,16 +1,30 @@
 package entity
 
-// User is
-type User struct {
-	UUID
+import "go-rest-echo/helper"
+
+// UserPayloadCreate is entity for validation on create
+type UserPayloadCreate struct {
 	Name     string `json:"name" validate:"required,min=5"`
 	Email    string `json:"email" validate:"required,email,min=5"`
 	Password string `json:"password" validate:"required,min=6"`
-	Task     []Task `json:"tasks"`
+}
+
+// User is
+type User struct {
+	UUID
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Task     []Task `json:"tasks,omitempty"`
 	Timestamps
 }
 
 // TableName is
 func (User) TableName() string {
 	return "users"
+}
+
+// SetID is
+func (User) SetID() string {
+	return helper.GenerateUUID()
 }
