04f56c3b2ea5825dfbf5fc0f1cd1a141d0c7fea7
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-24 16:42:04 +0700

    add validate tag and remove payload

--- internal/users/entity.go
+++ internal/users/entity.go
@@ -7,20 +7,12 @@ import (
 	"gorm.io/gorm"
 )
 
-// UserPayloadCreate is entity for validation on create
-type UserPayloadCreate struct {
-	ID       string `json:"id"`
-	Name     string `json:"name" validate:"required,min=5"`
-	Email    string `json:"email" validate:"required,email,min=5"`
-	Password string `json:"password" validate:"required,min=6"`
-}
-
 // User is
 type User struct {
 	ID        string         `json:"id"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email"`
-	Password  string         `json:"password"`
+	Name      string         `json:"name" validate:"required,min=5"`
+	Email     string         `json:"email" validate:"required,email,min=5"`
+	Password  string         `json:"password" validate:"required,min=6"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at"`
