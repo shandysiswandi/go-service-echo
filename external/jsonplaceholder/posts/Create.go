a592671cbca75277814e0c774eb1bfe8e6a9d982
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-25 20:05:40 +0700

    add Create Request posts

---
+++ external/jsonplaceholder/posts/Create.go
@@ -0,0 +1,30 @@
+package posts
+
+import (
+	"bytes"
+	"encoding/json"
+	"go-rest-echo/external/jsonplaceholder"
+	"net/http"
+)
+
+// Create is
+func Create(data Post) (*Post, error) {
+	post := new(Post)
+
+	reqBody, err := json.Marshal(data)
+	if err != nil {
+		return nil, jsonplaceholder.ErrPostCreate
+	}
+
+	response, err := http.Post(jsonplaceholder.PostsPath, "application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, jsonplaceholder.ErrPostCreate
+	}
+	defer response.Body.Close()
+
+	if err = json.NewDecoder(response.Body).Decode(post); err != nil {
+		return nil, jsonplaceholder.ErrPostCreate
+	}
+
+	return post, nil
+}
