b8083e9c73fb4a1b3235f8a95cf090f35c643eb8
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-25 23:12:12 +0700

    add request delete posts

---
+++ external/jsonplaceholder/posts/Delete.go
@@ -0,0 +1,30 @@
+package posts
+
+import (
+	"fmt"
+	"go-rest-echo/external/jsonplaceholder"
+	"net/http"
+)
+
+// Delete is
+func Delete(ID int) error {
+	link := fmt.Sprintf("%s/%d", jsonplaceholder.PostsPath, ID)
+	client := new(http.Client)
+
+	req, err := http.NewRequest(http.MethodDelete, link, nil)
+	if err != nil {
+		return jsonplaceholder.ErrPostDelete
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return jsonplaceholder.ErrPostDelete
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == 200 || response.StatusCode == 200 {
+		return nil
+	}
+
+	return jsonplaceholder.ErrPostDelete
+}
