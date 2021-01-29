d07ef65ced89f1e22dbd25c4fd60cbc1f95fc569
Shandy Siswandi <shandysiswandi@gmail.com>
2021-01-29 19:42:43 +0700

    rename error to success

--- app/context/error.go
+++ app/context/error.go
@@ -10,27 +10,35 @@ import (
 )
 
 type responseError struct {
-	Status  bool        `json:"status"`
+	Success bool        `json:"success"`
 	Message string      `json:"message"`
 	Error   interface{} `json:"error"`
 }
 
+func (c *CustomContext) commonError(code int, m string, e interface{}) error {
+	return c.JSON(code, responseError{
+		Success: false,
+		Message: m,
+		Error:   e,
+	})
+}
+
 // BadRequest is | 400
 func (c *CustomContext) BadRequest(err interface{}) error {
-	return c.JSON(http.StatusBadRequest, responseError{
-		Status:  false,
-		Message: "Bad Request, something wrong on your request",
-		Error:   err,
-	})
+	return c.commonError(
+		http.StatusBadRequest,
+		"Bad Request, something wrong on your request",
+		err,
+	)
 }
 
 // NotFound is | 404
 func (c *CustomContext) NotFound(err interface{}) error {
-	return c.JSON(http.StatusNotFound, responseError{
-		Status:  false,
-		Message: "Not Found, your request data not found in our database",
-		Error:   err,
-	})
+	return c.commonError(
+		http.StatusNotFound,
+		"Not Found, your request data not found in our database",
+		err,
+	)
 }
 
 // UnprocessableEntity is | 422
@@ -40,20 +48,20 @@ func (c *CustomContext) UnprocessableEntity(err interface{}) error {
 	for _, err := range err.(validator.ValidationErrors) {
 		e = append(e, map[string]interface{}{
 			"key":     util.SnakeCase(err.StructField()),
-			"message": getMessageValidation(err),
+			"message": c.getMessageValidation(err),
 			"value":   err.Value(),
 		})
 	}
 
-	return c.JSON(http.StatusUnprocessableEntity, responseError{
-		Status:  false,
-		Message: "Validation Failed",
-		Error:   e,
-	})
+	return c.commonError(
+		http.StatusUnprocessableEntity,
+		"Validation Failed",
+		e,
+	)
 }
 
 // private function to get message validation by flag | tag | reflect
-func getMessageValidation(e validator.FieldError) (msg string) {
+func (c *CustomContext) getMessageValidation(e validator.FieldError) (msg string) {
 	switch e.Tag() {
 	case "min":
 		msg = "value must be greater than"
@@ -72,14 +80,14 @@ func getMessageValidation(e validator.FieldError) (msg string) {
 
 // InternalServerError is | 500
 func (c *CustomContext) InternalServerError(err interface{}) error {
-	return c.JSON(http.StatusInternalServerError, responseError{
-		Status:  false,
-		Message: "Internal Server Error",
-		Error:   err,
-	})
+	return c.commonError(
+		http.StatusInternalServerError,
+		"Internal Server Error",
+		err,
+	)
 }
 
-// HandleErrors is | 40x - 50x
+// HandleErrors is | 4xx - 50x
 func (c *CustomContext) HandleErrors(e error) error {
 	if errors.Is(e, gorm.ErrRecordNotFound) {
 		return c.NotFound(gorm.ErrRecordNotFound)
