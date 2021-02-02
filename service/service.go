7e8e63d067b8fbea2cad957989c0d256ff27e1e8
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-02 09:20:29 +0700

    service redis

--- service/service.go
+++ service/service.go
@@ -2,26 +2,16 @@ package service
 
 import (
 	"go-rest-echo/config"
-
-	"github.com/go-redis/redis"
 )
 
 // Service is
 type Service struct {
-	Redis *redis.Client
+	Redis Redis
 }
 
 // New is
 func New(c *config.Config) *Service {
 	return &Service{
-		Redis: redisConnection(c),
+		Redis: NewRedis(c),
 	}
-}
-
-func redisConnection(c *config.Config) *redis.Client {
-	return redis.NewClient(&redis.Options{
-		Addr:     c.Service.Redis.Addr,
-		Password: c.Service.Redis.Password,
-		DB:       c.Service.Redis.Database,
-	})
 }
