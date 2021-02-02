d05f03fdae55a7fdabd9b59a4338d4fa99e2c82c
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-02 09:20:54 +0700

    refactor

--- internal/blogs/interface.go
+++ internal/blogs/interface.go
@@ -2,32 +2,34 @@ package blogs
 
 import "github.com/labstack/echo/v4"
 
-// BlogRepository is
-type BlogRepository interface {
-	Fetch() (*[]Blog, error)
-	Get(string) (*Blog, error)
+type (
+	// BlogRepository is
+	BlogRepository interface {
+		Fetch() (*[]Blog, error)
+		Get(string) (*Blog, error)
 
-	Create(*Blog) error
-	Update(*Blog, string) error
-	Delete(string) error
-}
+		Create(*Blog) error
+		Update(*Blog, string) error
+		Delete(string) error
+	}
 
-// Usecase is
-type Usecase interface {
-	Fetch() (*[]Blog, error)
-	Get(string) (*Blog, error)
+	// Usecase is
+	Usecase interface {
+		Fetch() (*[]Blog, error)
+		Get(string) (*Blog, error)
 
-	Create(*Blog) error
-	Update(*Blog, string) error
-	Delete(string) error
-}
+		Create(*Blog) error
+		Update(*Blog, string) error
+		Delete(string) error
+	}
 
-// BlogDelivery is
-type BlogDelivery interface {
-	Fetch(echo.Context) error
-	Get(echo.Context) error
+	// BlogDelivery is
+	BlogDelivery interface {
+		Fetch(echo.Context) error
+		Get(echo.Context) error
 
-	Create(echo.Context) error
-	Update(echo.Context) error
-	Delete(echo.Context) error
-}
+		Create(echo.Context) error
+		Update(echo.Context) error
+		Delete(echo.Context) error
+	}
+)
