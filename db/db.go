ae40b6a84cecfd032f46b3014cb3eb21de91cc07
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-06 20:05:11 +0700

    fix db & db_test

--- db/db.go
+++ db/db.go
@@ -1,17 +1,20 @@
 package db
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"go-rest-echo/config"
-	"time"
+	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+)
+
+var (
+	once sync.Once
+	db   *Database
+	err  error
+	errs []error
 )
 
 // Database is
@@ -23,109 +26,49 @@ type Database struct {
 
 // NewDatabase is
 func NewDatabase(config *config.Config) (*Database, []error) {
-	var (
-		err  error
-		errs []error
-		db   = new(Database)
-	)
-
 	if config == nil {
-		return db, append(errs, errors.New("Configuration is nil"))
+		return nil, append(errs, errors.New("Configuration is nil"))
 	}
 
 	if len(config.Database.Drivers) < 1 {
-		return db, append(errs, errors.New("This application not using any database"))
+		return nil, append(errs, errors.New("This application not using any database"))
 	}
 
-	for _, s := range config.Database.Drivers {
-		if s == "mysql" && config.Database.MysqlDSN != "" {
-			db.Mysql, err = mysqlConnection(config.Database.MysqlDSN)
-			if err != nil {
-				errs = append(errs, errors.New("Can't connect database mysql"))
-			}
-			continue
-		}
+	once.Do(func() {
+		db = new(Database)
 
-		if s == "postgresql" && config.Database.PostgresqlDSN != "" {
-			db.Postgresql, err = postgresqlConnection(config.Database.PostgresqlDSN)
-			if err != nil {
-				errs = append(errs, errors.New("Can't connect database postgresql"))
+		for _, s := range config.Database.Drivers {
+			if s == "mysql" && config.Database.MysqlDSN != "" {
+				db.Mysql, err = mysqlConnection(config.Database.MysqlDSN)
+				if err != nil {
+					errs = append(errs, errors.New("Can't connect database mysql"))
+				}
+				continue
 			}
-			continue
-		}
 
-		if s == "mongo" && config.Database.Mongo.URI != "" && config.Database.Mongo.DB != "" {
-			db.Mongo, err = mongoConnection(config.Database.Mongo.URI, config.Database.Mongo.DB)
-			if err != nil {
-				errs = append(errs, errors.New("Can't connect database mongo"))
+			if s == "postgresql" && config.Database.PostgresqlDSN != "" {
+				db.Postgresql, err = postgresqlConnection(config.Database.PostgresqlDSN)
+				if err != nil {
+					errs = append(errs, errors.New("Can't connect database postgresql"))
+				}
+				continue
 			}
-			continue
-		}
 
-		errs = append(errs, fmt.Errorf("schema database '%s' is not support", s))
-	}
+			if s == "mongo" && config.Database.Mongo.URI != "" && config.Database.Mongo.DB != "" {
+				db.Mongo, err = mongoConnection(config.Database.Mongo.URI, config.Database.Mongo.DB)
+				if err != nil {
+					errs = append(errs, errors.New("Can't connect database mongo"))
+				}
+				continue
+			}
+
+			errs = append(errs, fmt.Errorf("schema database '%s' is not support", s))
+		}
+	})
 
 	if len(errs) > 0 {
 		return db, errs
 	}
 
 	return db, nil
-}
-
-// mysql connection
-func mysqlConnection(dsn string) (db *gorm.DB, err error) {
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{PrepareStmt: true})
-	if err != nil {
-		return nil, err
-	}
-
-	// pooling connection
-	sqlCon, err := db.DB()
-	if err != nil {
-		return nil, err
-	}
-
-	sqlCon.SetMaxIdleConns(10 / 2)
-	sqlCon.SetMaxOpenConns(100 / 2)
-	sqlCon.SetConnMaxLifetime(time.Hour / 2)
-
-	return db, nil
-}
-
-// postgresql connection
-func postgresqlConnection(dsn string) (db *gorm.DB, err error) {
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	sqlCon, err := db.DB()
-	if err != nil {
-		return nil, err
-	}
-
-	sqlCon.SetMaxIdleConns(10 / 2)
-	sqlCon.SetMaxOpenConns(100 / 2)
-	sqlCon.SetConnMaxLifetime(time.Hour / 2)
-
-	return db, nil
-}
-
-// mongo connection
-func mongoConnection(uri, database string) (*mongo.Database, error) {
-	clientOptions := options.Client().ApplyURI(uri)
-	client, err := mongo.NewClient(clientOptions)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return client.Database(database), nil
 }
--- db/db_test.go
+++ db/db_test.go
@@ -1,99 +1,34 @@
 package db_test
 
 import (
-	"errors"
 	"go-rest-echo/config"
 	"go-rest-echo/db"
+	"log"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewDatabase_ParamConfigIsNil(t *testing.T) {
-	_, errs := db.NewDatabase(nil)
-
-	assert.Equal(t, errors.New("Configuration is nil"), errs[0], "must be error")
+func init() {
+	err := godotenv.Load(".env.test")
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
 
-func TestNewDatabase_ParamConfigDatabaseDriversIsEmpty(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{}
+func TestNewDatabase(t *testing.T) {
+	var actual, errs = db.NewDatabase(config.NewConfiguration())
 
-	_, errs := db.NewDatabase(conf)
+	assert.Equal(t, []error(nil), errs)
 
-	assert.Equal(t, errors.New("This application not using any database"), errs[0], "must be error")
-}
+	assert.Equal(t, nil, actual.Mysql.Error)
+	assert.Equal(t, int64(0), actual.Mysql.RowsAffected)
 
-func TestNewDatabase_ParamConfigDatabaseIsMysql_Error(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"mysql"}
-	conf.Database.MysqlDSN = "mysql"
+	assert.Equal(t, nil, actual.Postgresql.Error)
+	assert.Equal(t, int64(0), actual.Postgresql.RowsAffected)
 
-	_, errs := db.NewDatabase(conf)
+	assert.Equal(t, "go-rest-echo", actual.Mongo.Name())
 
-	assert.Equal(t, errors.New("Can't connect database mysql"), errs[0], "must be error")
-}
-
-func TestNewDatabase_ParamConfigDatabaseIsMysql_Success(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"mysql"}
-	conf.Database.MysqlDSN = "root:password@tcp(127.0.0.1:3500)/go-rest-echo?charset=utf8mb4&parseTime=True&loc=Local"
-
-	want, _ := db.NewDatabase(nil)
-	got, _ := db.NewDatabase(conf)
-
-	assert.NotEqual(t, want.Mysql, got.Mysql, "must be success")
-}
-
-func TestNewDatabase_ParamConfigDatabaseIsPostgresql_Error(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"postgresql"}
-	conf.Database.PostgresqlDSN = "postgresql"
-
-	_, errs := db.NewDatabase(conf)
-
-	assert.Equal(t, errors.New("Can't connect database postgresql"), errs[0], "must be error")
-}
-
-func TestNewDatabase_ParamConfigDatabaseIsPostgresql_Success(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"postgresql"}
-	conf.Database.PostgresqlDSN = "user=root password=password dbname=go-rest-echo host=127.0.0.1 port=5300 sslmode=disable TimeZone=Asia/Jakarta"
-
-	want, _ := db.NewDatabase(nil)
-	got, _ := db.NewDatabase(conf)
-
-	assert.NotEqual(t, want.Postgresql, got.Postgresql, "must be success")
-}
-
-func TestNewDatabase_ParamConfigDatabaseIsMongo_Error(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"mongo"}
-	conf.Database.Mongo.URI = "uri"
-	conf.Database.Mongo.DB = "db"
-
-	_, errs := db.NewDatabase(conf)
-
-	assert.Equal(t, errors.New("Can't connect database mongo"), errs[0], "must be error")
-}
-
-func TestNewDatabase_ParamConfigDatabaseIsMongo_Success(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"mongo"}
-	conf.Database.Mongo.URI = "mongodb://root:password@localhost:27017/?readPreference=primary&ssl=false"
-	conf.Database.Mongo.DB = "go-rest-echo"
-
-	want, _ := db.NewDatabase(nil)
-	got, _ := db.NewDatabase(conf)
-
-	assert.NotEqual(t, want.Mongo, got.Mongo, "must be success")
-}
-
-func TestNewDatabase_ParamConfigDatabaseDriversAreNotSupport(t *testing.T) {
-	conf := new(config.Config)
-	conf.Database.Drivers = []string{"not-support-driver"}
-
-	_, errs := db.NewDatabase(conf)
-
-	assert.Equal(t, errors.New("schema database 'not-support-driver' is not support"), errs[0], "must be error")
 }
