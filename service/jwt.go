0bfe7523bde0cb25583fcf5768b57cc4d0961a24
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-07 00:34:47 +0700

    add custom jwtClaim

--- service/jwt.go
+++ service/jwt.go
@@ -9,7 +9,6 @@ import (
 )
 
 type (
-
 	// JWT is
 	JWT struct {
 		config *config.Config
@@ -20,6 +19,19 @@ type (
 		AccessToken  string
 		RefreshToken string
 	}
+
+	// JWTClaimData is
+	JWTClaimData struct {
+		ID    string `json:"id"`
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	}
+
+	// JWTClaim is
+	JWTClaim struct {
+		Data JWTClaimData `json:"data"`
+		jwt.StandardClaims
+	}
 )
 
 // NewJWT is
@@ -28,32 +40,37 @@ func NewJWT(c *config.Config) *JWT {
 }
 
 // Generate is
-func (j *JWT) Generate(data jwt.MapClaims) (*JWTToken, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
+func (j *JWT) Generate(data JWTClaimData) (*JWTToken, error) {
 	genToken := new(JWTToken)
 	err := errors.New("Token Not Valid")
 
 	// access token
-	accClaim := token.Claims.(jwt.MapClaims)
-	accClaim["data"] = data
-	accClaim["aud"] = "ACCESS_TOKEN_AUDIENCE"
-	accClaim["exp"] = time.Now().Add(time.Minute * 15).Unix()
-	accClaim["iat"] = time.Now().Unix()
-	accClaim["iss"] = "ACCESS_TOKEN"
-	accClaim["sub"] = "ACCESS_TOKEN_SUBJECT"
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &JWTClaim{
+		data,
+		jwt.StandardClaims{
+			Audience:  "ACCESS_TOKEN_AUDIENCE",
+			ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "ACCESS_TOKEN_ISSUER",
+			Subject:   "ACCESS_TOKEN_SUBJECT",
+		},
+	})
 	genToken.AccessToken, err = token.SignedString(j.config.Service.JWT.AccessSecret)
 	if err != nil {
 		return nil, err
 	}
 
 	// refresh token
-	refClaim := token.Claims.(jwt.MapClaims)
-	refClaim["data"] = data
-	refClaim["aud"] = "REFRESH_TOKEN_AUDIENCE"
-	refClaim["exp"] = time.Now().Add(time.Hour * 24).Unix()
-	refClaim["iat"] = time.Now().Unix()
-	refClaim["iss"] = "REFRESH_TOKEN"
-	refClaim["sub"] = "REFRESH_TOKEN_SUBJECT"
+	token = jwt.NewWithClaims(jwt.SigningMethodHS256, &JWTClaim{
+		data,
+		jwt.StandardClaims{
+			Audience:  "REFRESH_TOKEN_AUDIENCE",
+			ExpiresAt: time.Now().Add(time.Minute * 24).Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "REFRESH_TOKEN_ISSUER",
+			Subject:   "REFRESH_TOKEN_SUBJECT",
+		},
+	})
 	genToken.RefreshToken, err = token.SignedString(j.config.Service.JWT.RefreshSecret)
 	if err != nil {
 		return nil, err
