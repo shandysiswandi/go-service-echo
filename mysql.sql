e3813dc69e2be3a51da68ae61c3259075ec39c3d
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-07 11:01:32 +0700

    create table blogs

--- mysql.sql
+++ mysql.sql
@@ -30,3 +30,13 @@ CREATE TABLE `tasks` (
   KEY `fk_user_tasks` (`user_id`),
   CONSTRAINT `fk_user_tasks` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- CREATED_AT = 07-02-2021 10:38:00
+-- create table `blogs` with id using uuid
+DROP TABLE IF EXISTS `blogs`;
+CREATE TABLE `blogs` (
+  `id` char(36) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
