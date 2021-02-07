3d99a4c455d524eb9eb0eacd23c4eff5eadb49c3
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-07 13:02:10 +0700

    add payload

--- internal/blogs/BlogEntity.go
+++ internal/blogs/BlogEntity.go
@@ -2,18 +2,32 @@ package blogs
 
 import "github.com/google/uuid"
 
-// BlogPayloadCreate is
-type BlogPayloadCreate struct {
-	Title string `json:"title" validate:"required,min=5"`
-	Body  string `json:"body" validate:"required,min=15"`
-}
+type (
+	// BlogPayloadCreate is
+	BlogPayloadCreate struct {
+		Title string `json:"title" validate:"required,min=5"`
+		Body  string `json:"body" validate:"required,min=15"`
+	}
 
-// Blog is
-type Blog struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
-}
+	// BlogPayloadPut is
+	BlogPayloadPut struct {
+		Title string `json:"title" validate:"required,min=5"`
+		Body  string `json:"body" validate:"required,min=15"`
+	}
+
+	// BlogPayloadPatch is
+	BlogPayloadPatch struct {
+		Title string `json:"title,omitempty" validate:"min=5"`
+		Body  string `json:"body,omitempty" validate:"min=15"`
+	}
+
+	// Blog is
+	Blog struct {
+		ID    string `json:"id"`
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	}
+)
 
 // TableName is
 func (Blog) TableName() string {
