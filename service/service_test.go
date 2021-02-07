82aa952c3b5fc1bd9e1c2fe4fd2a6733676adbf9
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-07 22:03:31 +0700

    update service test

--- service/service_test.go
+++ service/service_test.go
@@ -1,7 +1,19 @@
 package service_test
 
-import "testing"
+import (
+	"go-rest-echo/config"
+	"go-rest-echo/service"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNew(t *testing.T) {
-	//
+	c := config.NewConfiguration()
+	service := service.New(c)
+
+	assert.NotNil(t, service)
+	assert.NotNil(t, service.JWT)
+	assert.NotNil(t, service.Redis)
+	assert.Nil(t, service.Sentry)
 }
