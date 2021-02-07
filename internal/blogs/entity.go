9c5fd591b83341cc8c518882ba9a9524a2327a2f
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-07 11:01:57 +0700

    add payload create and method setID

--- internal/blogs/entity.go
+++ internal/blogs/entity.go
@@ -1,5 +1,13 @@
 package blogs
 
+import "github.com/google/uuid"
+
+// BlogPayloadCreate is
+type BlogPayloadCreate struct {
+	Title string `json:"title" validate:"required,min=5"`
+	Body  string `json:"body" validate:"required,min=15"`
+}
+
 // Blog is
 type Blog struct {
 	ID    string `json:"id"`
@@ -10,4 +18,9 @@ type Blog struct {
 // TableName is
 func (Blog) TableName() string {
 	return "blogs"
+}
+
+// SetID is
+func (u *Blog) SetID() {
+	u.ID = uuid.New().String()
 }
