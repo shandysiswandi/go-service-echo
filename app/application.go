7d4995982d7758757e0b7a91ffc2380be913b4ac
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-12 18:14:50 +0700

    remove service & external param

--- app/application.go
+++ app/application.go
@@ -5,20 +5,19 @@ import (
 	"go-rest-echo/app/validation"
 	"go-rest-echo/config"
 	"go-rest-echo/db"
-	"go-rest-echo/external"
-	"go-rest-echo/service"
 
 	"github.com/labstack/echo/v4"
 )
 
 // NewApplicationAndServe is
-func NewApplicationAndServe(c *config.Config, db *db.Database, s *service.Service, ex *external.External) {
+func NewApplicationAndServe(c *config.Config, db *db.Database) {
 	e := echo.New()
 
 	context.New(e)
 	validation.New(e)
+
 	middlewares(e)
-	routes(e, c, db, s, ex)
+	routes(e, c, db)
 
 	e.Logger.Fatal(e.Start(":" + c.App.Port))
 }
