f085b44ef62c4623071339b5234a9c33f183a132
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-13 16:10:10 +0700

    improve db

--- db/db.go
+++ db/db.go
@@ -4,17 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"go-rest-echo/config"
-	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"gorm.io/gorm"
-)
-
-var (
-	once sync.Once
-	db   *Database
-	err  error
-	errs []error
 )
 
 // Database is
@@ -26,6 +18,11 @@ type Database struct {
 
 // NewDatabase is
 func NewDatabase(config *config.Config) (*Database, []error) {
+	var (
+		errs []error
+		err  error
+	)
+
 	if config == nil {
 		return nil, append(errs, errors.New("Configuration is nil"))
 	}
@@ -34,37 +31,35 @@ func NewDatabase(config *config.Config) (*Database, []error) {
 		return nil, append(errs, errors.New("This application not using any database"))
 	}
 
-	once.Do(func() {
-		db = new(Database)
+	db := new(Database)
 
-		for _, s := range config.Database.Drivers {
-			if s == "mysql" && config.Database.MysqlDSN != "" {
-				db.Mysql, err = mysqlConnection(config.Database.MysqlDSN)
-				if err != nil {
-					errs = append(errs, errors.New("Can't connect database mysql"))
-				}
-				continue
+	for _, s := range config.Database.Drivers {
+		if s == "mysql" && config.Database.MysqlDSN != "" {
+			db.Mysql, err = mysqlConnection(config.Database.MysqlDSN)
+			if err != nil {
+				errs = append(errs, errors.New("Can't connect database mysql"))
 			}
-
-			if s == "postgresql" && config.Database.PostgresqlDSN != "" {
-				db.Postgresql, err = postgresqlConnection(config.Database.PostgresqlDSN)
-				if err != nil {
-					errs = append(errs, errors.New("Can't connect database postgresql"))
-				}
-				continue
-			}
-
-			if s == "mongo" && config.Database.Mongo.URI != "" && config.Database.Mongo.DB != "" {
-				db.Mongo, err = mongoConnection(config.Database.Mongo.URI, config.Database.Mongo.DB)
-				if err != nil {
-					errs = append(errs, errors.New("Can't connect database mongo"))
-				}
-				continue
-			}
-
-			errs = append(errs, fmt.Errorf("schema database '%s' is not support", s))
+			continue
 		}
-	})
+
+		if s == "postgresql" && config.Database.PostgresqlDSN != "" {
+			db.Postgresql, err = postgresqlConnection(config.Database.PostgresqlDSN)
+			if err != nil {
+				errs = append(errs, errors.New("Can't connect database postgresql"))
+			}
+			continue
+		}
+
+		if s == "mongo" && config.Database.Mongo.URI != "" && config.Database.Mongo.DB != "" {
+			db.Mongo, err = mongoConnection(config.Database.Mongo.URI, config.Database.Mongo.DB)
+			if err != nil {
+				errs = append(errs, errors.New("Can't connect database mongo"))
+			}
+			continue
+		}
+
+		errs = append(errs, fmt.Errorf("schema database '%s' is not support", s))
+	}
 
 	if len(errs) > 0 {
 		return db, errs
