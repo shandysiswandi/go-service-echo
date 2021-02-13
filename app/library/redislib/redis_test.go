82f912a5247a853e54ce062aacbe2e3bda71774e
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-13 21:06:28 +0700

    clean redis

--- app/library/redislib/redis_test.go
+++ app/library/redislib/redis_test.go
@@ -10,12 +10,65 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	if err := godotenv.Load(".env"); err != nil {
-		t.Error("no .env file")
+	is := assert.New(t)
+
+	if err := godotenv.Load(".redis"); err != nil {
+		is.Nil(err)
 	}
 
-	is := assert.New(t)
 	redis := redislib.New(config.New())
 
-	is.Nil(redis)
+	is.NotNil(redis)
+}
+
+func TestNew_Get(t *testing.T) {
+	is := assert.New(t)
+
+	if err := godotenv.Load(".redis"); err != nil {
+		is.Nil(err)
+	}
+
+	redis := redislib.New(config.New())
+	err := redis.Get()
+
+	is.Nil(err)
+}
+
+func TestNew_Set(t *testing.T) {
+	is := assert.New(t)
+
+	if err := godotenv.Load(".redis"); err != nil {
+		is.Nil(err)
+	}
+
+	redis := redislib.New(config.New())
+	err := redis.Set()
+
+	is.Nil(err)
+}
+
+func TestNew_Increment(t *testing.T) {
+	is := assert.New(t)
+
+	if err := godotenv.Load(".redis"); err != nil {
+		is.Nil(err)
+	}
+
+	redis := redislib.New(config.New())
+	err := redis.Increment()
+
+	is.Nil(err)
+}
+
+func TestNew_Decrement(t *testing.T) {
+	is := assert.New(t)
+
+	if err := godotenv.Load(".redis"); err != nil {
+		is.Nil(err)
+	}
+
+	redis := redislib.New(config.New())
+	err := redis.Decrement()
+
+	is.Nil(err)
 }
