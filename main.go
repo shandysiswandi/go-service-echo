754821135e7f2e7c7410d64f459e39488c5c4073
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-13 20:08:18 +0700

    clean main.go

--- main.go
+++ main.go
@@ -4,22 +4,20 @@ import (
 	"go-rest-echo/app"
 	"go-rest-echo/config"
 	"go-rest-echo/db"
-	"go-rest-echo/util"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	if err := godotenv.Load(".env"); err != nil {
-		panic("NO .env GUYS")
+		print(err)
 	}
 
 	config := config.New()
-	db, errs := db.NewDatabase(config)
-	if errs != nil {
-		for _, e := range errs {
-			util.LogError(e)
-		}
+
+	db, err := db.NewDatabase(config)
+	if err != nil {
+		print(err)
 	}
 
 	app.NewApplicationAndServe(config, db)
