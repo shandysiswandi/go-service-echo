26740db3823191cf1ba80726df1a3189a4b96f95
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-26 09:43:15 +0700

    improve: main.go

--- main.go
+++ main.go
@@ -7,19 +7,37 @@ import (
 	"go-service-echo/util/logger"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	/********** ********** ********** **********/
+	/* Load environment
+	/********** ********** ********** **********/
 	if err := godotenv.Load(".env"); err != nil {
 		logger.Error(err)
 	}
 
+	/********** ********** ********** **********/
+	/* Define config variable
+	/********** ********** ********** **********/
 	config := config.New()
 
+	/********** ********** ********** **********/
+	/* Define database variable
+	/********** ********** ********** **********/
 	db, err := db.New(config.Database)
 	if err != nil {
 		logger.Error(err)
 	}
 
-	app.New(config, db)
+	/********** ********** ********** **********/
+	/* Define server and app variable then run it
+	/********** ********** ********** **********/
+	app.New(echo.New(), config, db).
+		SetContext().
+		SetValidation().
+		SetMiddlewares().
+		SetRoutes().
+		Run()
 }
