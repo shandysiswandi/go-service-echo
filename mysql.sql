cd95db75357eba0af2306874841063af4c8dfc1b
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-27 14:55:56 +0700

    change: sql ddl

--- mysql.sql
+++ mysql.sql
@@ -18,30 +18,3 @@ CREATE TABLE `users` (
 INSERT INTO users(id, name, email, password)
 VALUES ('190e9c30-c119-40a2-a554-85b6fb8cffaf', 'admin', 'admin@admin.com',
         '$2y$10$M.VrmGnEdrJyYyYTGmkynexSg9MjHDhPckyyVJvxoZGRAOMKDL3fq ');
-
--- CREATED_AT = 17-01-2021 12:12:02
--- create table `tasks` with id using uuid
-DROP TABLE IF EXISTS `tasks`;
-CREATE TABLE `tasks` (
-  `id` char(36) NOT NULL,
-  `user_id` varchar(36) DEFAULT NULL,
-  `title` varchar(100) NOT NULL,
-  `description` varchar(1000) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_tasks_deleted_at` (`deleted_at`),
-  KEY `fk_user_tasks` (`user_id`),
-  CONSTRAINT `fk_user_tasks` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- CREATED_AT = 07-02-2021 10:38:00
--- create table `blogs` with id using uuid
-DROP TABLE IF EXISTS `blogs`;
-CREATE TABLE `blogs` (
-  `id` char(36) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `body` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- postgresql.sql
+++ postgresql.sql
@@ -16,27 +16,3 @@ CREATE TABLE public.users
 INSERT INTO users(id, name, email, password)
 VALUES ('190e9c30-c119-40a2-a554-85b6fb8cffaf', 'admin', 'admin@admin.com',
         '$2y$10$M.VrmGnEdrJyYyYTGmkynexSg9MjHDhPckyyVJvxoZGRAOMKDL3fq ');
-
--- CREATED_AT = 17-01-2021 12:12:01
--- create table `tasks` with id using uuid
-DROP TABLE IF EXISTS public.tasks;
-CREATE TABLE public.tasks
-(
-    id char(36) not null constraint tasks_pkey primary key,
-    user_id char(36) default null,
-    title varchar(100) not null,
-    description varchar(1000) not null,
-    created_at timestamp default now(),
-    updated_at timestamp default now(),
-    deleted_at timestamp
-);
-
--- CREATED_AT = 07-02-2021 10:38:00
--- create table `blogs` with id using uuid
-DROP TABLE IF EXISTS public.blogs;
-CREATE TABLE public.blogs
-(
-    id char(36) not null constraint blogs_pkey primary key,
-    title varchar(100) not null,
-    body varchar(100) not null
-);
