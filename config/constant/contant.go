a7d28f0aa4eb389e3027d3759204c8d16255a115
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-27 12:57:00 +0700

    change: to package constant

---
+++ config/constant/contant.go
@@ -0,0 +1,20 @@
+package constant
+
+// Version and Build is
+const (
+	Version string = "1.0.0"
+	Build   string = "0.0.1"
+)
+
+// environment
+const (
+	Development string = "development"
+	Testing     string = "testing"
+	Production  string = "production"
+)
+
+// token
+const (
+	JWT    string = "jwt"
+	Paseto string = "paseto"
+)
--- config/contant.go
+++
@@ -1,7 +0,0 @@
-package config
-
-// Version and Build is
-const (
-	Version string = "1.0.0"
-	Build   string = "0.0.1"
-)
