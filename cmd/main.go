03f04cfbb432a1393ad2d30b7606e76e1b4ba3e7
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-28 23:20:19 +0700

    create: cmd/main.go

---
+++ cmd/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"go-service-echo/config"
+	"go-service-echo/db"
+	"go-service-echo/internal/users"
+	"go-service-echo/util/bcrypt"
+	"go-service-echo/util/faker"
+	"go-service-echo/util/logger"
+	"os"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		logger.Error(err)
+	}
+
+	config := config.New()
+
+	database, err := db.New(config.Database)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	go doBatch(database, 100000)
+
+	// time.Sleep(time.Second / 2)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+}
+
+func doBatch(database *db.Database, l int) {
+	pass, _ := bcrypt.HashPassword("password")
+
+	for i := 0; i < l; i++ {
+		data := &users.User{
+			ID:       uuid.New().String(),
+			Name:     faker.Name(),
+			Email:    faker.Email(),
+			Password: pass,
+		}
+
+		if err := database.SQL.Create(data).Error; err != nil {
+			logger.Error(err)
+		}
+	}
+}
