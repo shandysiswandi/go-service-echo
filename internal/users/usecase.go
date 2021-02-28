4ae5a9426152d0775defb6b122c794a43f56a848
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-28 23:48:57 +0700

    change: implement interface usecase

--- internal/users/usecase.go
+++ internal/users/usecase.go
@@ -1,37 +1,35 @@
 package users
 
-// UserUsecase is
-type UserUsecase struct {
+type userUsecase struct {
 	userRepository UserRepository
 }
 
 // NewUserUsecase is
-func NewUserUsecase(r UserRepository) *UserUsecase {
-	return &UserUsecase{userRepository: r}
+func NewUserUsecase(ur UserRepository) UserUsecase {
+	return &userUsecase{userRepository: ur}
 }
 
-// Fetch is
-func (u *UserUsecase) Fetch() (Users, error) {
+func (u *userUsecase) Fetch() (Users, error) {
 	return u.userRepository.Fetch()
 }
 
-// Get is
-func (u *UserUsecase) Get(ID string) (*User, error) {
+func (u *userUsecase) Get(ID string) (*User, error) {
 	return u.userRepository.Get(ID)
 }
 
-// Create is
-func (u *UserUsecase) Create(eu *User) error {
+func (u *userUsecase) GetByEmail(email string) (*User, error) {
+	return u.userRepository.GetByEmail(email)
+}
+
+func (u *userUsecase) Create(eu *User) error {
 	eu.SetID()
 	return u.userRepository.Create(eu)
 }
 
-// Update is
-func (u *UserUsecase) Update(t *User, ID string) error {
+func (u *userUsecase) Update(t *User, ID string) error {
 	return u.userRepository.Update(t, ID)
 }
 
-// Delete is
-func (u *UserUsecase) Delete(ID string) error {
+func (u *userUsecase) Delete(ID string) error {
 	return u.userRepository.Delete(ID)
 }
