532ecdc4535436e575a54cb6ca42cc57be603d3b
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-08 12:44:35 +0700

    cleam: arrays_test

--- util/arrays/arrays_test.go
+++ util/arrays/arrays_test.go
@@ -3,18 +3,12 @@ package arrays_test
 import (
 	"go-service-echo/util/arrays"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestInArray(t *testing.T) {
+func Test_InArray(t *testing.T) {
 	list := []string{"1", "2", "3", "4", "5"}
-	act := arrays.InArray(list, "2")
-	act2 := arrays.InArray(list, "0")
-
-	if true != act {
-		t.Error("must same")
-	}
-
-	if false != act2 {
-		t.Error("must same")
-	}
+	assert.Equal(t, true, arrays.InArray(list, "2"))
+	assert.Equal(t, false, arrays.InArray(list, "0"))
 }
