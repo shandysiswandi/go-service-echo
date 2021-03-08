1bf5e29fb7eaf45c12f161dd82c9d4c908fac667
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-08 12:57:14 +0700

    update: util/stringy & add RandomString

--- util/stringy/stringy.go
+++ util/stringy/stringy.go
@@ -1,8 +1,16 @@
 package stringy
 
 import (
+	"math/rand"
 	"regexp"
 	"strings"
+	"time"
+)
+
+const (
+	charsetLower  = "abcdefghijklmnopqrstuvwxyz"
+	charsetUpper  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	charsetNumber = "0123456789"
 )
 
 // Split is
@@ -18,11 +26,19 @@ func Split(a, b string) []string {
 
 // SnakeCase is
 func SnakeCase(str string) string {
-	var (
-		matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-		matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
-	)
+	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
+}
+
+// RandomString is
+func RandomString(l int) string {
+	b := make([]byte, l)
+	for i := range b {
+		b[i] = charsetLower[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(charsetLower))]
+	}
+	return string(b)
 }
--- util/stringy/stringy_test.go
+++ util/stringy/stringy_test.go
@@ -7,40 +7,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSnakeCase(t *testing.T) {
-	is := assert.New(t)
-
-	cases := []struct {
-		desc     string
-		input    string
-		expected string
-	}{
-		{"All text is capitalize only one word", "CAPITALIZE", "capitalize"},
-		{"All text is lower with one word", "lower", "lower"},
-		{"Text input is IsUUID", "IsUUID", "is_uuid"},
-		{"Text input is UserId", "UserId", "user_id"},
-		{"Text input is theURL", "theURL", "the_url"},
-	}
-
-	for _, tc := range cases {
-		is.Equal(tc.expected, stringy.SnakeCase(tc.input))
-	}
+func Test_SnakeCase(t *testing.T) {
+	assert.Equal(t, "capitalize", stringy.SnakeCase("CAPITALIZE"))
+	assert.Equal(t, "lower", stringy.SnakeCase("lower"))
+	assert.Equal(t, "is_uuid", stringy.SnakeCase("IsUUID"))
+	assert.Equal(t, "user_id", stringy.SnakeCase("UserId"))
+	assert.Equal(t, "the_url", stringy.SnakeCase("theURL"))
 }
 
-func TestSplit(t *testing.T) {
-	is := assert.New(t)
+func Test_Split(t *testing.T) {
+	assert.Equal(t, []string{"a", "2"}, stringy.Split("a/2", "/"))
+	assert.Equal(t, []string{"a", "2"}, stringy.Split("/a/2", "/"))
+	assert.Equal(t, []string{"a", "2"}, stringy.Split("/a/2/", "/"))
+}
 
-	cases := []struct {
-		desc     string
-		inA, inB string
-		expected []string
-	}{
-		{"test 1", "a/2", "/", []string{"a", "2"}},
-		{"test 2", "/a/2", "/", []string{"a", "2"}},
-		{"test 3", "/a/2/", "/", []string{"a", "2"}},
-	}
-
-	for _, tc := range cases {
-		is.Equal(tc.expected, stringy.Split(tc.inA, tc.inB))
-	}
+func Test_RandomString(t *testing.T) {
+	assert.Equal(t, 10, len(stringy.RandomString(10)))
+	assert.Equal(t, 10, len(stringy.RandomString(10)))
+	assert.Equal(t, 50, len(stringy.RandomString(50)))
+	assert.Equal(t, 100, len(stringy.RandomString(100)))
 }
