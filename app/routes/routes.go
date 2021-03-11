07db2d55f5d58e56bcbd67f5459c8ba9315894cc
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-11 15:16:34 +0700

    update: routes

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -7,43 +7,49 @@ import (
 	"go-service-echo/domain"
 	"go-service-echo/domain/authentication"
 	"go-service-echo/domain/users"
-	"go-service-echo/infrastructure/database"
+	"go-service-echo/infrastructure/gormdb"
 	"go-service-echo/infrastructure/jsonplaceholder"
 
 	"github.com/labstack/echo/v4"
 )
 
-// Routes is
-type Routes struct {
-	engine *echo.Echo
-	prefix *echo.Group
-}
+type (
+	// Routes is
+	Routes struct {
+		Engine          *echo.Echo
+		Database        *gormdb.Database
+		Token           *token.Token
+		Redis           *redis.Redis
+		Sentry          *sentry.Sentry
+		JSONPlaceHolder *jsonplaceholder.JSONPlaceHolder
+	}
+)
 
 // New is
-func New(e *echo.Echo) *Routes {
-	p := e.Group("/api")
-	return &Routes{e, p}
-}
+func New(r *Routes) *Routes {
+	/* define variables */
+	api := r.Engine.Group("/api")
+	db := r.Database
+	token := r.Token
+	redis := r.Redis
+	sentry := r.Sentry
+	jsonPlaceHolder := r.JSONPlaceHolder
 
-// Default is
-func (r *Routes) Default(db *database.Database, tok *token.Token, red *redis.Redis, sen *sentry.Sentry, jph *jsonplaceholder.JSONPlaceHolder) *Routes {
-	dHanlder := domain.NewDefaultHandler(db, tok, red, sen, jph)
+	/* Route Default */
+	dhc := &domain.DefaultHandlerConfig{Database: db, Token: token, Redis: redis, Sentry: sentry, JSONPlaceHolder: jsonPlaceHolder}
+	dHanlder := domain.NewDefaultHandler(dhc)
 
-	r.engine.GET("/", dHanlder.Default)                                  // default route and check | db | token | sentry
-	r.engine.Any("/cors", dHanlder.CORS)                                 // for cors testing
-	r.engine.GET("/favicon.ico", dHanlder.Favicon)                       // for request via browser
-	r.engine.GET("/example-external-call", dHanlder.ExampleExternalCall) // example external call
+	r.Engine.GET("/", dHanlder.Default)                                  // default route and check | db | token | sentry
+	r.Engine.Any("/cors", dHanlder.CORS)                                 // for cors testing
+	r.Engine.GET("/favicon.ico", dHanlder.Favicon)                       // for request via browser
+	r.Engine.GET("/example-external-call", dHanlder.ExampleExternalCall) // example external call
 
-	return r
-}
-
-// Auth is
-func (r *Routes) Auth(db *database.Database, tok *token.Token) *Routes {
+	/* Route Auth */
 	userRepo := users.NewGormRepository(db.SQL)
-	authUsecase := authentication.NewUsecase(userRepo, tok)
+	authUsecase := authentication.NewUsecase(userRepo, token)
 	authHanlder := authentication.NewHandler(authUsecase)
 
-	authRoute := r.engine.Group("/auth")
+	authRoute := r.Engine.Group("/auth")
 	authRoute.POST("/login", authHanlder.Login)
 	authRoute.POST("/register", authHanlder.Login)
 	authRoute.POST("/forgot-password", authHanlder.Login)
@@ -51,21 +57,17 @@ func (r *Routes) Auth(db *database.Database, tok *token.Token) *Routes {
 	authRoute.POST("/refresh", authHanlder.Login)
 	authRoute.POST("/logout", authHanlder.Login)
 
-	return r
-}
-
-// Users is
-func (r *Routes) Users(db *database.Database) *Routes {
-	userRepo := users.NewGormRepository(db.SQL)
+	/* Route Users */
 	userUsecase := users.NewUserUsecase(userRepo)
 	userHanlder := users.NewUserHandler(userUsecase)
 
-	usersRoute := r.prefix.Group("/users")
+	usersRoute := api.Group("/users")
 	usersRoute.GET("", userHanlder.Fetch)
 	usersRoute.GET("/:id", userHanlder.Get)
 	usersRoute.POST("", userHanlder.Create)
 	usersRoute.PUT("/:id", userHanlder.Update)
 	usersRoute.DELETE("/:id", userHanlder.Delete)
 
-	return r
+	/* return */
+	return nil
 }
