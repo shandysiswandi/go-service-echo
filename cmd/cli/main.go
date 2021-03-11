e8042db79a8080dbf63c3ec617184feff15c9578
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-11 15:26:00 +0700

    update: cmd/cli/main.go

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"go-service-echo/config"
 	"go-service-echo/domain/users"
 	"go-service-echo/infrastructure/gormdb"
-	"go-service-echo/util/bcrypt"
-	"go-service-echo/util/faker"
-	"os"
+	"go-service-echo/util/security"
 
+	"github.com/bxcodec/faker/v3"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
@@ -25,18 +22,22 @@ func main() {
 	if err != nil {
 		println("Err: ", err)
 	}
+	println(database)
 
-	go doBatch(database, 100000)
+	// go doBatch(database, 100000)
+
+	// go dumMD5(database)
 
 	// time.Sleep(time.Second / 2)
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter text: ")
+	// text, _ := reader.ReadString('\n')
+	// fmt.Println(text)
+	// fmt.Scanln()
 }
 
 func doBatch(database *gormdb.Database, l int) {
-	pass, _ := bcrypt.HashPassword("password")
+	pass := security.Password("password")
 
 	for i := 0; i < l; i++ {
 		data := &users.User{
