51790dc103fe8ad116de5aa8b0f2200d673192bc
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-11 15:16:52 +0700

    add: validateVar

--- app/validation/validation.go
+++ app/validation/validation.go
@@ -1,6 +1,9 @@
 package validation
 
 import (
+	"fmt"
+	"go-service-echo/app/response"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
@@ -10,11 +13,31 @@ type valid struct {
 }
 
 // New is
-func New(e *echo.Echo) *echo.Echo {
+func New(e *echo.Echo) {
 	e.Validator = &valid{validator: validator.New()}
-	return e
 }
 
 func (v *valid) Validate(i interface{}) error {
 	return v.validator.Struct(i)
 }
+
+// ValidateVar is
+func ValidateVar(value interface{}, tag string) map[string]interface{} {
+	var v = validator.New()
+	var e map[string]interface{}
+
+	if err := v.Var(value, tag); err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			e = map[string]interface{}{"message": fmt.Sprintf("`%v` %s", err.Value(), response.GetMessageValidation(err))}
+		}
+
+		return e
+	}
+
+	return nil
+}
+
+// func (c *CustomContext) GetTokenData() token.PayloadData {
+// 	tokPayload := c.Get("user").(*token.Payload)
+// 	return tokPayload.Data
+// }
